c0beb52dc67876b1d061395fe91ae1f7b64f2cbe
dartrhevan <vovasatunkin@mail.ru>
2019-05-19 10:32:06 +0500

    Final Version

--- Web/Controllers/EditorsController.cs
+++ Web/Controllers/EditorsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Xml.Serialization;
@@ -26,12 +27,18 @@ namespace Web.Controllers
                     throw new ArgumentException();
             }
         }
-        public IActionResult PlanetEditor(string name)
+
+        public IActionResult PlanetEditor(string name, string owner, string gal)
         {
-            //var pl = new DBPlanet();
-            //st.Planets.Add(pl);
-            return View(name);
+            var l = new List<DBMoon>();
+            SaveToSession(l, null, "moons");
+
+            return View("PlanetEditor", Tuple.Create(name, owner, gal));
         }
+
+        public IActionResult MoonEditor(string name, string owner, string gal) =>
+            View(Tuple.Create(name, owner, gal));
+
 
         [HttpPost]
         public IActionResult AddStar(IFormFile Photo, string Name, string Galaxy, uint Radius, uint Temperature, DateTime Date, uint Dist, string Unit)
@@ -44,34 +51,24 @@ namespace Web.Controllers
                 str.Read(ph, 0, ph.Length);
                 str.Close();
             }
-
             var dbs = new DBStar(Date, ph, Name, new Distance(Dist, StringToUnit(Unit)), Radius, Temperature, Galaxy);
             var xml = new XmlSerializer(typeof(List<DBPlanet>));
             var stream = new MemoryStream(HttpContext.Session.Get("planets"));
-            //xml.Serialize(str, l);
             var planets = xml.Deserialize(stream) as List<DBPlanet>;
             foreach (var planet in planets)
                 dbs.Planets.Add(planet);
             using (var db = new AstronomicDirectoryDbContext())
             {
-                //if (db.Stars.FirstOrDefault(s => s.Name == st.Name) == null)
-                //{
                 db.Stars.Add(dbs);
                 db.Planets.AddRange(dbs.Planets);
                 db.Moons.AddRange(dbs.Planets.SelectMany(pl => pl.Moons));
-                //foreach (var pl in dbs.Planets)
-                //    if(pl.Moons != null)
-
                 db.SaveChanges();
-                //}
-
             }
-
             return View("~/Views/Views/StarView.cshtml", dbs);
         }
 
         [HttpPost]
-        public IActionResult AddPlanet(IFormFile Photo, string Name, string Galaxy, uint Radius, uint Temperature, DateTime Date, uint Dist, string Unit)
+        public IActionResult AddMoon(IFormFile Photo, string Name, string Galaxy, uint Radius, uint Temperature, DateTime Date, uint Dist, string Unit)
         {
             byte[] ph = new byte[0];
             if (Photo != null)
@@ -81,30 +78,48 @@ namespace Web.Controllers
                 str.Read(ph, 0, ph.Length);
                 str.Close();
             }
-
-            //var l = new List<DBPlanet>();
-            var xml = new XmlSerializer(typeof(List<DBPlanet>));
-            var stream = new MemoryStream(HttpContext.Session.Get("planets"));
-            //xml.Serialize(str, l);
-            var planets = xml.Deserialize(stream) as List<DBPlanet>;
-
+            var xml = new XmlSerializer(typeof(List<DBMoon>));
+            var stream = new MemoryStream(HttpContext.Session.Get("moons"));
+            var moons = xml.Deserialize(stream) as List<DBMoon>;
             using (var db = new AstronomicDirectoryDbContext())
             {
-                //var st = db.Stars.Find(StId);
-                var planet = new DBPlanet(Date, ph, Name, new Distance(Dist, StringToUnit(Unit)), Radius, false, PlanetType.Gas, "", Galaxy, Temperature);
+                var moon = new DBMoon(Date, ph, Name, new Distance(Dist, StringToUnit(Unit)), Radius, false, PlanetType.Gas, "", Galaxy, Temperature);
+                moons.Add(moon);
+                SaveToSession(moons, xml, "moons");
+                return View("~/Views/Views/MoonView.cshtml", moon);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult AddPlanet(IFormFile Photo, string Name, string Galaxy, uint Radius, uint Temperature, DateTime Date, uint Dist, string Unit, bool atm, bool type)
+        {
+            byte[] ph = new byte[0];
+            if (Photo != null)
+            {
+                var str = Photo.OpenReadStream();
+                ph = new byte[str.Length];
+                str.Read(ph, 0, ph.Length);
+                str.Close();
+            }
+            var xml = new XmlSerializer(typeof(List<DBPlanet>));
+            var stream = new MemoryStream(HttpContext.Session.Get("planets"));
+            var planets = xml.Deserialize(stream) as List<DBPlanet>;
+            using (var db = new AstronomicDirectoryDbContext())
+            {
+                var planet = new DBPlanet(Date, ph, Name, new Distance(Dist, StringToUnit(Unit)), Radius, atm, type?PlanetType.Tought : PlanetType.Gas, "", Galaxy, Temperature);
+
+                var mx = new XmlSerializer(typeof(List<DBMoon>));
+                var st = new MemoryStream(HttpContext.Session.Get("moons"));
+                var moons = mx.Deserialize(st) as List<DBMoon>;
+                planet.Moons = new Collection<DBMoon>(moons);
+
                 planets.Add(planet);
-
-                //var l = new List<DBPlanet>();
-                //var xml = new XmlSerializer(l.GetType());
-                //var str = new MemoryStream();
-                //xml.Serialize(str, planets);
-                //HttpContext.Session.Set("planets", str.ToArray());
                 SaveToSession(planets, xml);
-
                 return View("~/Views/Views/PlanetView.cshtml", planet);
             }
-
         }
+
         public IActionResult StarEditor()
         {
             var l = new List<DBPlanet>();
@@ -113,15 +128,12 @@ namespace Web.Controllers
             return View(star);
         }
 
-        private void SaveToSession(List<DBPlanet> l, XmlSerializer x = null)
+        private void SaveToSession(object l, XmlSerializer x = null, string key = "planets")
         {
             var xml = x ?? new XmlSerializer(l.GetType());
             var str = new MemoryStream();
             xml.Serialize(str, l);
-            HttpContext.Session.Set("planets", str.ToArray());
+            HttpContext.Session.Set(key, str.ToArray());
         }
-
     }
-
-
 }
--- Web/Controllers/ViewsController.cs
+++ Web/Controllers/ViewsController.cs
@@ -8,6 +8,8 @@ using AstronomicDirectory;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using Web.Models.DataAccessMySqlProvider;
 
 namespace Web.Controllers
@@ -62,6 +64,157 @@ namespace Web.Controllers
                 //{ }
                 return View(dbs);
             }
+        }
+
+        public ActionResult Print(int id)
+        {
+            using (var db = new AstronomicDirectoryDbContext())
+            {
+                var star = db.Stars.Include(s1 => s1.Planets).First(s1 => s1.Id == id);
+                db.Moons.Load();
+                IWorkbook workbook =
+                    new XSSFWorkbook(System.IO.File.OpenRead("template.xlsx"));
+
+                var sheet = workbook.GetSheetAt(0);
+
+                sheet.GetRow(1).Cells[1].SetCellValue(star.Name);
+
+                PrintStar(star, sheet);
+
+                var ms = new MemoryStream();
+                workbook.Write(ms);
+
+                ms.Position = 0;
+
+                return base.File(ms, "application/octet-stream", "star" + id + ".xlsx");
+            }
+        }
+
+        private static void PrintStar(DBStar star, ISheet sheet)
+        {
+            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+            {
+                IRow row = sheet.GetRow(i);
+                if (row == null) continue;
+                if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                var lastCellNum = row.LastCellNum;
+                for (int j = row.FirstCellNum; j < lastCellNum; j++)
+                {
+                    var cell = row.GetCell(j);
+                    if (cell != null)
+                    {
+                        if (cell.StringCellValue == "$PropRow")
+                        {
+                            PrintStarProp(star, sheet, ref i, row, j, cell);
+
+                            break;
+                        }
+
+                        if (cell.StringCellValue == "$Planets")
+                        {
+                            foreach (var planet in star.Planets)
+                            //{
+                                PrintPlanets(star, sheet, i++, row, j, cell, planet);
+                            //    i++;
+                            //}
+
+                            break;
+                        }
+
+                        if (cell.StringCellValue == "$Moons")
+                        {
+                            foreach (var moon in star.Planets.SelectMany(p => p.Moons))
+                                //{
+                                PrintMoons(star, sheet, i++, row, j, cell, moon);
+                            //    i++;
+                            //}
+
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private static void PrintMoons(DBStar star, ISheet sheet, int i, IRow row, int j, ICell cell, DBMoon moon)
+        {
+            row = sheet.GetRow(i);
+            cell = row.GetCell(j);
+            cell.SetCellValue(moon.Name);
+            cell = row.GetCell(j + 1) ?? row.CreateCell(j + 1);
+            cell.SetCellValue(moon.MiddleDistanceValue +
+                              moon.MiddleDistanceUnit.ToString());
+            cell = row.GetCell(j + 2) ?? row.CreateCell(j + 2);
+            cell.SetCellValue(moon.Radius);
+            cell = row.GetCell(j + 3) ?? row.CreateCell(j + 3);
+            cell.SetCellValue(moon.PlanetOwner);
+            cell = row.GetCell(j + 4) ?? row.CreateCell(j + 4);
+            cell.SetCellValue(moon.Temperature);
+            cell = row.GetCell(j + 5) ?? row.CreateCell(j + 5);
+            cell.SetCellValue(moon.InventingDate);
+            cell.CellStyle.DataFormat = 14;
+
+            cell = row.GetCell(j + 6) ?? row.CreateCell(j + 6);
+            cell.SetCellValue(moon.HasAtmosphere);/*
+            if (planet != star.Planets.Last())
+                row = sheet.CopyRow(i, i + 1);*/
+        }
+
+
+        private static void PrintStarProp(DBStar star, ISheet sheet, ref int i, IRow row, int j, ICell cell)
+        {
+            cell.SetCellValue("Дата открытия");
+            cell = row.GetCell(j + 1) ?? row.CreateCell(j + 1);
+            cell.SetCellValue(star.InventingDate);
+            cell.CellStyle.DataFormat = 14;
+
+            row = sheet.CopyRow(i, i + 1);
+            i++;
+            cell = row.GetCell(j) ?? row.CreateCell(j);
+            cell.SetCellValue("Галактика");
+            row.CreateCell(j + 1).SetCellValue(star.Galaxy ?? "");
+            row = sheet.CreateRow(i++);
+            cell = row.GetCell(j) ?? row.CreateCell(j);
+            cell.SetCellValue("Радиус");
+            row.CreateCell(j + 1).SetCellValue(star.Radius);
+            row = sheet.CreateRow(i++);
+
+            cell = row.GetCell(j) ?? row.CreateCell(j);
+            cell.SetCellValue("Температура");
+            row.CreateCell(j + 1).SetCellValue(star.Temperature);
+            row = sheet.CreateRow(i++);
+
+            cell = row.GetCell(j) ?? row.CreateCell(j);
+            cell.SetCellValue("Среднее расстояние");
+            row.CreateCell(j + 1).SetCellValue(star.MiddleDistanceValue);
+            row = sheet.CreateRow(i++);
+        }
+
+        private static void PrintPlanets(DBStar star, ISheet sheet, int i, IRow row, int j, ICell cell, DBPlanet planet)
+        {
+            row = sheet.GetRow(i);
+            cell = row.GetCell(j);
+            cell.SetCellValue(planet.Name);
+            cell = row.GetCell(j + 1) ?? row.CreateCell(j + 1);
+            cell.SetCellValue(planet.MiddleDistanceValue +
+                              planet.MiddleDistanceUnit.ToString());
+            cell = row.GetCell(j + 2) ?? row.CreateCell(j + 2);
+            cell.SetCellValue(planet.Radius);
+            cell = row.GetCell(j + 3) ?? row.CreateCell(j + 3);
+            cell.SetCellValue(planet.Star);
+            cell = row.GetCell(j + 4) ?? row.CreateCell(j + 4);
+            cell.SetCellValue(planet.Temperature);
+            cell = row.GetCell(j + 5) ?? row.CreateCell(j + 5);
+            cell.SetCellValue(planet.Type.ToString());
+            cell = row.GetCell(j + 6) ?? row.CreateCell(j + 6);
+            cell.SetCellValue(planet.InventingDate);
+            cell.CellStyle.DataFormat = 14;
+
+            cell = row.GetCell(j + 7) ?? row.CreateCell(j + 7);
+            cell.SetCellValue(planet.HasAtmosphere);
+            if (planet != star.Planets.Last())
+                row = sheet.CopyRow(i, i + 1);
         }
 
         public FileContentResult DownloadStar(int id)
